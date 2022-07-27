361a9f2a1932176cf0ab65e09f26ef8b2ab4e09f
Omar-Hosny1 <omarhosny810@yahoo.com>
2022-07-27 20:23:11 +0200

    Output Component Added

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import OutPut from "./OutPut";
 
 function Greeting() {
   const [changedText, setChangedText] = useState(false);
@@ -8,8 +9,8 @@ function Greeting() {
   return (
     <div>
       <h1>Hello World!</h1>
-      {!changedText && <p>It's Good To See You!</p>}
-      {changedText && <p>Changed</p>}
+      {!changedText && <OutPut>It's Good To See You!</OutPut>}
+      {changedText && <OutPut>Changed</OutPut>}
       <button onClick={changeTextHandler}>Change Text!</button>
     </div>
   );
