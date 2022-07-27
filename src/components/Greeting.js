0a7474f4382e12c270745e5b76953e124ffdb1a8
Omar-Hosny1 <omarhosny810@yahoo.com>
2022-07-27 20:18:47 +0200

    Test Created For Greeting Component

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Greeting() {
+  const [changedText, setChangedText] = useState(false);
+  const changeTextHandler = () => {
+    setChangedText(true);
+  };
   return (
     <div>
       <h1>Hello World!</h1>
-      <p>It's Good To See You!</p>
+      {!changedText && <p>It's Good To See You!</p>}
+      {changedText && <p>Changed</p>}
+      <button onClick={changeTextHandler}>Change Text!</button>
     </div>
   );
 }
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 test("tset hello world", () => {
   //Arrenge
@@ -9,4 +10,30 @@ test("tset hello world", () => {
   //Assert
   const helloWorldEle = screen.getByText("Hello World", { exact: false });
   expect(helloWorldEle).toBeInTheDocument();
+});
+
+test("Is Paragraph Shown", () => {
+  render(<Greeting />);
+  const ParagraphEle = screen.getByText("It's Good To See You!", {
+    exact: true,
+  });
+  expect(ParagraphEle).toBeInTheDocument();
+});
+
+test("render Changed", () => {
+  render(<Greeting />);
+  const btnEle = screen.getByText("Change Text!");
+  userEvent.click(btnEle);
+
+  const Paragraph = screen.getByText("Changed", { exact: false });
+  expect(Paragraph).toBeInTheDocument();
+});
+
+test("First Paragraph is not shown", () => {
+  render(<Greeting />);
+  const btn = screen.getByRole("button");
+  userEvent.click(btn);
+
+  const FirstPa = screen.queryByText("It's Good To See You!");
+  expect(FirstPa).toBeNull;
 });
